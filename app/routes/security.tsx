eb385558ee1ffff859f82cbcda3eb7fdae5d5b84
Jamie Legg <legg.jamie@gmail.com>
2022-03-10 14:25:33 +0000

    added source

--- app/routes/security.tsx
+++ app/routes/security.tsx
@@ -1,12 +1,13 @@
 import Typist from "react-typist";
+import Button from "~/components/button";
 import Heading from "~/components/heading";
 import Nav from "~/components/nav";
 
 const Security = () => {
     return (
-        <div className="bg-gradient-to-r text-white from-slate-900 to-fuchsia-900 h-screen w-full">
-            <Nav />
-            <div className="py-10 mx-20 rounded-md">
+        <Layout>
+            <NavBar />
+            <div className="py-10 mx-20 rounded-md min-h-screen">
                 <div className="flex">
                     <Heading><Typist>
                         <Typist.Delay ms={1000} />
@@ -17,7 +18,9 @@ const Security = () => {
                 <div className="mt-3">
                     <div className="mt-3">Your personal information is encrypted and protected by open source security standards.</div>
                     <div className="mt-3">The source code is available to view at all times on GitHub.</div>
+                    <Button><a href="https://github.com/jamie-legg/pegaport">VIEW SOURCE</a></Button>
                 </div>
+
                 <div className="mt-12">
                     <Heading>
                         <Typist>
@@ -27,6 +30,7 @@ const Security = () => {
 
                     <div className="mt-3">You will never be charged to use Pegaport, nor will we ever run ads.</div>
                     <div className="mt-3">You can support the project by becoming a priveleged member and gaining additional features.</div>
+                    <Button>SUPPORT PEGAPORT</Button>
                 </div>
 
             </div>
